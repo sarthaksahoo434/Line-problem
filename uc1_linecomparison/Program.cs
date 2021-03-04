9d2bc26254debbe8d0e75973d5ff5bb99aa6873d
SarthaksaHOO434 <Sarthaksahoo434@gmail.com>
2021-03-04 18:31:36 +0530

    [newfloder]ADD:Line-problem

---
+++ uc1_linecomparison/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uc1_linecomparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1,x2,y1,y2 = 0;
+            Console.Write("Input an integer value: +x1 +x2 +y1 +y2 ");
+            x1= Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double distance;
+            distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("distance =" + distance);
+
+
+
+        }
+    }
+}
